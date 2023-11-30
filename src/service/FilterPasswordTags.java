6ffa1c9a2b2989ef6121f188e37bbfbe4755cf17
S S Cheema <ssc26@student.le.ac.uk>
2023-11-30 14:40:20 +0000

    Add backbones and delete FavouritePasswordFilter class. Add UpdateUserEmail and it's backbone.
    Added backbones which are methods for DeleteAccount, FilterPasswordTags, OnetimePassword, PasswordEncryption,PasswordStrengthRating. Added UpdateUserEmail and it's backbone.

--- src/service/FilterPasswordTags.java
+++ src/service/FilterPasswordTags.java
@@ -1,2 +1,12 @@
 public class FilterPasswordTags {
+
+    // Constructor
+    public FilterPasswordTags() {
+        // initialisation
+    }
+
+    // Method to filter passwords by tag
+    public void filterByTag(String tag) {
+        // Logic to filter passwords by the given tag
+    }
 }
